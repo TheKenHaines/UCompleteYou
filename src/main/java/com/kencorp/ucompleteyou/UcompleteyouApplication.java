c45f56a6e2bba650fbb48aab6e25a01eaed19e48
Kenneth Haines <TheKenHaines@gmail.com>
2022-04-22 23:11:24 -0500

    Added Project

---
+++ src/main/java/com/kencorp/ucompleteyou/UcompleteyouApplication.java
@@ -0,0 +1,13 @@
+package com.kencorp.ucompleteyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcompleteyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcompleteyouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kencorp/ucompleteyou/UcompleteyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kencorp.ucompleteyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcompleteyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
